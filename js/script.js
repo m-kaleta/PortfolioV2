99afe89d07cfae74a033fd0e0af8a472b581b218
Mateusz Kaleta <m.kaleta9696@gmail.com>
2020-07-11 23:52:10 +0200

    added Portfolio and Skills SECTION

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const nav = document.querySelector(".navbar")
 
-
 addShadow = () => {
     if(window.scrollY >= 300) {
         nav.classList.add("shadow-bg")
@@ -10,6 +9,34 @@ addShadow = () => {
     }
 }
 
+$('.portfolio-carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 3500,
+    mobileFirst: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+    {
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 2
+        }
+    },
+    {
+        breakpoint: 992,
+        settings: {
+            slidesToShow: 3
+        }
+    },
+    {
+        breakpoint: 1600,
+        settings: {
+            slidesToShow: 4
+        }
+    }, 
+]
+});
+  
 window.addEventListener("scroll", addShadow)
 
 })
