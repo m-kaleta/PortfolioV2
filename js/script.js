abadfd220b8fc55d891bfc20f9d79655e1f51dbd
Mateusz Kaleta <m.kaleta9696@gmail.com>
2020-07-14 17:02:31 +0200

    added JS to the form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,53 @@
+const userName = document.querySelector('#username')
+const email = document.querySelector('#email')
+const text = document.querySelector('#message')
+
+const sendBtn = document.querySelector('.send')
+const clearBtn = document.querySelector('.clear')
+
+
+const showError = (input, msg) => {
+    const formBox = input.parentElement;
+    const errorMsg = formBox.querySelector(".error-text")
+
+    formBox.classList.add("error")
+    errorMsg.textContent = msg;
+}
+ 
+const clearError = input => {
+    const formBox = input.parentElement;
+    formBox.classList.remove("error")
+}
+
+const checkLabel = input => {
+    input.forEach(el => {
+        if(el.value === "") {
+            showError(el, el.placeholder)
+        } else {
+            clearError(el)
+            console.log("ok;)")
+        }
+    })
+}
+
+
+sendBtn.addEventListener("click", (e) => {
+
+    checkLabel([userName, email, text])
+})
+
+clearBtn.addEventListener("click", (e) => {
+
+    [userName, email, text].forEach(el => {
+        el.value = ""
+        clearError(el)
+    })
+
+})
+
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
     const nav = document.querySelector(".navbar")
 
@@ -36,7 +86,7 @@ $('.portfolio-carousel').slick({
     }, 
 ]
 });
-  
+
 window.addEventListener("scroll", addShadow)
 
 })
