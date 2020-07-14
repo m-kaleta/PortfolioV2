f4f773927389641f5239303623b0d0112076ccde
Mateusz Kaleta <m.kaleta9696@gmail.com>
2020-07-14 22:47:28 +0200

    added card "PortfolioV2" and finished FORM with JS.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
+
+// FORM
+
 const userName = document.querySelector('#username')
 const email = document.querySelector('#email')
 const text = document.querySelector('#message')
+const success = document.querySelector(".success")
 
 const sendBtn = document.querySelector('.send')
 const clearBtn = document.querySelector('.clear')
@@ -25,28 +29,70 @@ const checkLabel = input => {
             showError(el, el.placeholder)
         } else {
             clearError(el)
-            console.log("ok;)")
         }
     })
 }
 
+const checkEmail = email => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.value === "") {
+        showError()
+    }
+
+    if(re.test(email.value)) {
+        clearError(email) 
+    } else {
+        showError(email, "Email zawiera niedozwolone znaki!")
+    }
+}
+
+const checkLength = (input, min) => {
+    if(input.value.length < min) {
+        showError(input, `${input.previousElementSibling.innerText.slice(0, -1)} musi składać sie z min. ${min} znaków.`)
+    }
+}
+
+const clearSucces = () => {
+    success.classList.remove("success2")
+}
+
+const clearLabels = () => {
+    [userName, email, text].forEach(el => {
+        el.value = ""
+    })
+}
+
+const checkErrors = () => {
+    const inputs = document.querySelectorAll(".form-box")
+    errors = 0
+
+    inputs.forEach(el => {
+        if(el.classList.contains("error")) {
+            errors++
+        }
+    })
+
+    if(errors === 0) {
+        success.classList.add("success2")
+        clearLabels()
+    }
+}
 
 sendBtn.addEventListener("click", (e) => {
 
     checkLabel([userName, email, text])
+    checkLength(userName, 4)
+    checkLength(text, 8)
+    checkEmail(email)
+    checkErrors()
 })
 
 clearBtn.addEventListener("click", (e) => {
-
-    [userName, email, text].forEach(el => {
-        el.value = ""
-        clearError(el)
-    })
-
+    clearLabels()
+    clearSucces()
 })
 
-
-
+// NAV
 
 document.addEventListener("DOMContentLoaded", () => {
     const nav = document.querySelector(".navbar")
@@ -58,6 +104,8 @@ addShadow = () => {
         nav.classList.remove("shadow-bg")
     }
 }
+
+// SLIDER PORTFOLIO
 
 $('.portfolio-carousel').slick({
     autoplay: true,
